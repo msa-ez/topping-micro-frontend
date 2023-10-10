e2446c63b8924afe07ef13535dece9d05921a834
dhtns818 <dhtns818@rosesystems.kr>
2023-10-10 11:28:17 +0900

    commit

--- for-model/microfrontend-root/webpack.config.js
+++ for-model/microfrontend-root/webpack.config.js
@@ -1,6 +1,6 @@
 forEach: Model
 fileName: webpack.config.js
-path: for-model/microfrontend-root
+path: microfrontend-root
 ---
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
